cfd358aa71016a2dc27adccf5b0ef356149d89e0
MasonMcKayla <iameyl@gmail.com>
2026-03-20 15:42:51 -0400

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,15 +1,20 @@
+// [ engine.js ] 변수 선언 및 물리 엔진
 var scene, camera, renderer, controls, currentEgg, ghostEgg;
-var isPlacementMode = false;
-const BASE_H = 20, BASE_R = 7;
+var isPlacementMode = false, dragTarget = null, isDragging = false;
+var rotX_H, rotY_H, rotZ_H, handles = [];
+var groupGizmo, groupHandles = [], groupRotHandles = [];
+var startScale, startPos, startRotation, mouseButton, startGroupData = [], lastMouseAngle = 0;
+var worldAnchorPos = new THREE.Vector3(), startIntersect = new THREE.Vector3(), startHandleDist = 1;
+var isBoxSelecting = false, boxStart = new THREE.Vector2();
+var history = [], redoStack = [], currentSelection = [];
+const CM_RATIO = 0.5, BASE_H = 20, BASE_R = 7, H_OFFSET = 7;
 
+// 아버님의 3.5제곱 변형 로직 (원본 그대로)
 function updateMesh(egg) {
-    const pos = egg.geometry.attributes.position;
-    const orig = egg.geometry.userData.origPos;
-    const { scale, offsets } = egg.userData.data;
+    const pos = egg.geometry.attributes.position, orig = egg.geometry.userData.origPos, { scale, offsets } = egg.userData.data;
     const ctrls = [new THREE.Vector3(0,10,0), new THREE.Vector3(0,-10,0), new THREE.Vector3(-7,0,0), new THREE.Vector3(7,0,0), new THREE.Vector3(0,0,7), new THREE.Vector3(0,0,-7)];
     for (let i = 0; i < orig.count; i++) {
-        const v = new THREE.Vector3(orig.getX(i), orig.getY(i), orig.getZ(i));
-        const sV = v.clone().multiply(scale);
+        const v = new THREE.Vector3(orig.getX(i), orig.getY(i), orig.getZ(i)), sV = v.clone().multiply(scale);
         let tw = 0; const sOff = new THREE.Vector3();
         for (let j = 0; j < 6; j++) {
             const w = 1 / Math.pow(v.distanceTo(ctrls[j]) + 1, 3.5);
@@ -20,6 +25,7 @@ function updateMesh(egg) {
     pos.needsUpdate = true;
 }
 
+// 알 생성 (원본 그대로)
 function createEgg(color, isGhost = false) {
     const points = [];
     for (let i = 0; i <= 20; i++) {
