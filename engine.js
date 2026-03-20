7c84a7e58e04fc3936282b430dc6c3a759d36dcd
MasonMcKayla <iameyl@gmail.com>
2026-03-20 15:47:58 -0400

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,17 +1,12 @@
-// [ engine.js ] v5.48.11 Core Physics Engine
 var scene, camera, renderer, controls, currentEgg, ghostEgg;
 var isPlacementMode = false, dragTarget = null, isDragging = false;
-var rotX_H, rotY_H, rotZ_H, handles = [];
-var groupGizmo, groupHandles = [], groupRotHandles = [];
-var startScale, startPos, startRotation, mouseButton, startGroupData = [], lastMouseAngle = 0;
-var worldAnchorPos = new THREE.Vector3(), startIntersect = new THREE.Vector3(), startHandleDist = 1;
-var isBoxSelecting = false, boxStart = new THREE.Vector2();
-var history = [], redoStack = [], currentSelection = [];
-const CM_RATIO = 0.5, BASE_H = 20, BASE_R = 7, H_OFFSET = 7;
+var handles = [], rotX_H, rotY_H, rotZ_H;
+var currentSelection = [], history = [], redoStack = [];
+const BASE_H = 20, BASE_R = 7;
 
-// 아버님의 핵심: 3.5제곱 가중치 변형 로직
 function updateMesh(egg) {
-    const pos = egg.geometry.attributes.position, orig = egg.geometry.userData.origPos, { scale, offsets } = egg.userData.data;
+    const pos = egg.geometry.attributes.position, orig = egg.geometry.userData.origPos;
+    const { scale, offsets } = egg.userData.data;
     const ctrls = [new THREE.Vector3(0,10,0), new THREE.Vector3(0,-10,0), new THREE.Vector3(-7,0,0), new THREE.Vector3(7,0,0), new THREE.Vector3(0,0,7), new THREE.Vector3(0,0,-7)];
     for (let i = 0; i < orig.count; i++) {
         const v = new THREE.Vector3(orig.getX(i), orig.getY(i), orig.getZ(i)), sV = v.clone().multiply(scale);
